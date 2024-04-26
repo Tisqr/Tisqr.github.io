8a327ec3a0b81dcf0dcd8c17e0f2e8245821375d
muhammadali360 <devillishcoconuts@gmail.com>
2024-04-26 16:39:50 +0500

    page improvements + faster loading times

--- components/header.js
+++ components/header.js
@@ -1,17 +1,17 @@
-document.write(`
- <nav class="navbar navbar-expand-lg bg-primary px-2 py-3 shadow" data-bs-theme="dark">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="/">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link" href="./projects.html">Projects</a>
-            <a class="nav-link" href="./about.html">About</a>
-          </div>
-        </div>
-      </div>
-    </nav> 
-    `
-    )
+var navbar = document.getElementById('navbar');
+navbar.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-primary px-2 py-3 shadow" data-bs-theme="dark">
+<div class="container-fluid">
+<a class="navbar-brand" href="/">Home</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+    <a class="nav-link" href="./projects.html">Projects</a>
+    <a class="nav-link" href="./about.html">About</a>
+  </div>
+</div>
+</div>
+</nav> 
+`
